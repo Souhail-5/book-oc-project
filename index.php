06bdaf299e70f026fc3da4087c384a750c4dba46
Souhail Moussaoui <smsouhail@gmail.com>
2017-06-29 03:02:55 +0200

    Add homepage structure

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Billet simple pour l'Alaska</title>
+	<link rel="stylesheet" href="vendors/normalize/normalize.css">
+	<link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+	<section>
+		<figure>
+			<img src="" alt="Book's cover">
+		</figure>
+		<div>
+			<div class="admin-bar">
+				<span>Nouvel épisode</span>
+			</div>
+			<nav>
+				<ul>
+					<li>
+						<div>
+							<span># 23</span>
+							<h2>Titre</h2>
+						</div>
+						<div>
+							<time datetime="YYYY-MM-DD">Date</time>
+							<a href="">2 commentaires</a>
+						</div>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</section>
+</body>
+</html>
