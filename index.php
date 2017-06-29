4a9e23939fa789b7225b4da46a5e37297f04cacc
Souhail Moussaoui <smsouhail@gmail.com>
2017-06-29 16:58:57 +0200

    Update homepage style with icons

--- index.php
+++ index.php
@@ -4,37 +4,44 @@
 	<meta charset="UTF-8">
 	<title>Billet simple pour l'Alaska</title>
 	<link href="https://fonts.googleapis.com/css?family=Inconsolata:700|Lato:300,400|Merriweather:300" rel="stylesheet">
+	<link rel="stylesheet" href="vendors/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="vendors/normalize/normalize.css">
 	<link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
-	<section class="intro">
+	<div class="section-intro">
 		<div class="container">
 			<figure class="poster">
 				<img src="assets/images/poster-le-roi-lion.jpg" alt="Book's poster" class="cover">
 			</figure>
-			<div class="back-poster">
+			<section class="back-poster">
 				<div class="admin-bar">
-					<span>Nouvel épisode</span>
+					<span class="act-new"><i class="fa fa-plus" aria-hidden="true"></i> Nouvel épisode</span>
 				</div>
 				<nav class="episodes-card">
 					<ul>
-						<?php for ($i=0; $i < 20; $i++): ?>
+						<?php for ($i=23; $i > 0; $i--): ?>
 						<li>
 							<div class="episode-header">
-								<span># 23</span>
+								<span># <?php echo $i;?></span>
 								<h2>There will be blood</h2>
 							</div>
 							<div class="episode-metadata">
-								<time datetime="YYYY-MM-DD">Publié le 26 juin 2017</time>
-								<a href="">2 commentaires</a>
+								<div>
+									<time datetime="YYYY-MM-DD"><i class="fa fa-calendar" aria-hidden="true"></i> 26 juin 2017</time>
+									<a href="#"><i class="fa fa-comments-o" aria-hidden="true"></i> 2 commentaires</a>
+								</div>
+								<div>
+									<span class="act-delete"><i class="fa fa-trash-o" aria-hidden="true"></i> Supprimer</span>
+									<span class="act-edit"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Modifier</span>
+								</div>
 							</div>
 						</li>
 						<?php endfor; ?>
 					</ul>
 				</nav>
-			</div>
+			</section>
 		</div>
-	</section>
+	</div>
 </body>
 </html>
