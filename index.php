b06535b6eb1f509a8f30aae38ebc6becd071613a
Souhail Moussaoui <smsouhail@gmail.com>
2017-06-29 04:56:10 +0200

    Update homepage structure and style

--- index.php
+++ index.php
@@ -7,28 +7,32 @@
 	<link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
-	<section>
-		<figure>
-			<img src="" alt="Book's cover">
-		</figure>
-		<div>
-			<div class="admin-bar">
-				<span>Nouvel épisode</span>
+	<section class="intro">
+		<div class="container">
+			<figure class="poster">
+				<img src="assets/images/poster-le-roi-lion.jpg" alt="Book's poster" class="cover">
+			</figure>
+			<div class="back-poster">
+				<div class="admin-bar">
+					<span>Nouvel épisode</span>
+				</div>
+				<nav class="episodes-card">
+					<ul>
+						<?php for ($i=0; $i < 20; $i++): ?>
+						<li>
+							<div class="episode-header">
+								<span># 23</span>
+								<h2>There will be blood</h2>
+							</div>
+							<div class="episode-metadata">
+								<time datetime="YYYY-MM-DD">Publié le 26 juin 2017</time>
+								<a href="">2 commentaires</a>
+							</div>
+						</li>
+						<?php endfor; ?>
+					</ul>
+				</nav>
 			</div>
-			<nav>
-				<ul>
-					<li>
-						<div>
-							<span># 23</span>
-							<h2>Titre</h2>
-						</div>
-						<div>
-							<time datetime="YYYY-MM-DD">Date</time>
-							<a href="">2 commentaires</a>
-						</div>
-					</li>
-				</ul>
-			</nav>
 		</div>
 	</section>
 </body>
