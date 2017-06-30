7a3a839c4269a82f38546898a266e9091bde5583
Souhail Moussaoui <smsouhail@gmail.com>
2017-06-30 20:15:20 +0200

    Add template engine

---
+++ models/Template.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+* Template manager
+*/
+class Template
+{
+	private $_path;
+	private $_data;
+
+	public function __construct($path, array $data=array())
+	{
+		$this->_path = $path;
+		$this->_data = $data;
+	}
+
+	public function __set($property, $value)
+	{
+		return $this->_data[$property] = $value;
+	}
+
+	public function __get($property)
+	{
+		return array_key_exists($property, $this->_data) ? $this->_data[$property] : null;
+	}
+
+	public function render()
+	{
+		if (file_exists('views/'.$this->_path.'.php')) {
+			extract($this->_data);
+
+			ob_start();
+				include('views/'.$this->_path.'.php');
+				$rendering = ob_get_contents();
+			@ob_end_clean();
+
+			return $rendering;
+		} else {
+			//Throws exception
+		}
+	}
+}
