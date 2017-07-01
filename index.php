d525543795e4cadba2e14ec553885e63f82350f0
Souhail Moussaoui <smsouhail@gmail.com>
2017-07-01 03:19:09 +0200

    Add index.php as a kernel

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+include('models/Template.php');
+
+$currentParthUrl = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+
+$page = new Template('page');
+$page->language = "fr";
+$page->scripts = array(
+	'#',
+);
+
+if ($currentParthUrl[0] == 'episodes') {
+	$page->title = "Billet simple pour l'Alaska";
+	$page->stylesheets = array(
+		'https://fonts.googleapis.com/css?family=Inconsolata:700|Lato:300,400|Merriweather:300',
+		'/vendors/font-awesome-4.7.0/css/font-awesome.min.css',
+		'/vendors/normalize/normalize.css',
+		'/assets/css/main.css',
+	);
+
+	$episodes = new Template('episodes');
+	$episodes->test = 'Nouvel épisode';
+
+	$page->view = $episodes->render();
+
+	echo $page->render();
+}
+
+if ($currentParthUrl[0] == 'episode') {
+	$page->title = "Nom de l'épisode";
+	$page->stylesheets = array(
+		'https://fonts.googleapis.com/css?family=Inconsolata:700|Lato:300,400|Merriweather:300',
+		'/vendors/font-awesome-4.7.0/css/font-awesome.min.css',
+		'/vendors/normalize/normalize.css',
+		'/assets/css/episode.css',
+	);
+
+	$episode = new Template('episode');
+
+	$page->view = $episode->render();
+
+	echo $page->render();
+}
