e400477fec4f8831d2b58cf1d7788d5641b84bfb
Souhail Moussaoui <smsouhail@gmail.com>
2017-07-09 00:39:07 +0200

    Update clean up comments

--- index.php
+++ index.php
@@ -3,7 +3,3 @@ require_once 'helpers/class-autoloader.php';
 
 $router = new Router(new HTTPRequest);
 $router->runController();
-
-/*highlight_string("<?php\n\$data =\n" . var_export($router->route, true) . ";\n?>");
-echo "<br><br><br>";
-highlight_string("<?php\n\$data =\n" . var_export($_GET, true) . ";\n?>");*/
