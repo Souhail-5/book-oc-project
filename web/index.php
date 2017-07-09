674ff443b01fc9bd6a6218b75c6b170957c9b186
Souhail Moussaoui <smsouhail@gmail.com>
2017-07-09 18:58:42 +0200

    Add Psr-4 autoloader

---
+++ web/index.php
@@ -0,0 +1,24 @@
+<?php
+use \QFram\Helper\Psr4AutoloaderClass;
+use \QFram\Router;
+use \QFram\HTTPRequest;
+
+require_once __DIR__.'/../library/QFram/helpers/Psr4AutoloaderClass.php';
+
+// instantiate the loader
+$loader = new Psr4AutoloaderClass;
+
+// register the autoloader
+$loader->register();
+
+// register the base directories for the namespace prefix
+$loader->addNamespace('QFram', __DIR__.'/../library/QFram');
+$loader->addNamespace('QFram\Helper', __DIR__.'/../library/QFram/helpers');
+$loader->addNamespace('Model\Object', __DIR__.'/../app/models/domain-objects');
+$loader->addNamespace('Model\Mapper', __DIR__.'/../app/models/data-mappers');
+$loader->addNamespace('Model\Service', __DIR__.'/../app/models/services');
+$loader->addNamespace('View', __DIR__.'/../app/views');
+$loader->addNamespace('Controller', __DIR__.'/../app/controllers');
+
+$router = new Router(new HTTPRequest);
+$router->runController();
