0cb6f1359abab7545c7aefeb0ace76652236842d
Souhail Moussaoui <smsouhail@gmail.com>
2017-07-10 18:55:39 +0200

    Update episodes view with dynamic data

--- app/views/episodes.php
+++ app/views/episodes.php
@@ -12,13 +12,13 @@
 					<?php foreach ($episodes as $episode): ?>
 					<li>
 						<div class="episode-header">
-							<span># <?= $episode->number(); ?></span>
-							<h2><?= $episode->title(); ?></h2>
+							<span># <?= $episode->number() ?></span>
+							<h2><a href="<?= $path('episode', [$episode->number(), $episode->slug()]) ?>"><?= $episode->title() ?></a></h2>
 						</div>
 						<div class="episode-metadata">
 							<div>
-								<time datetime="YYYY-MM-DD"><i class="fa fa-calendar" aria-hidden="true"></i> <?= $episode->publish_datetime(); ?></time>
-								<a href="#"><i class="fa fa-comments-o" aria-hidden="true"></i> <?= $episode->nbr_comments(); ?> commentaires</a>
+								<time datetime="YYYY-MM-DD"><i class="fa fa-calendar" aria-hidden="true"></i> <?= $episode->publish_datetime() ?></time>
+								<a href="#"><i class="fa fa-comments-o" aria-hidden="true"></i> <?= $episode->nbr_comments() ?> commentaires</a>
 							</div>
 							<div>
 								<span class="act-delete"><i class="fa fa-trash-o" aria-hidden="true"></i> Supprimer</span>
