5c8853476bd60da510b6c16d88ee289cdc439f67
Souhail Moussaoui <smsouhail@gmail.com>
2017-07-12 18:29:02 +0200

    Fix show action of Episode Controller

--- app/controllers/Episode.php
+++ app/controllers/Episode.php
@@ -14,14 +14,21 @@ class Episode extends Controller
 {
 	public function show()
 	{
-		$this->page->title = "Nom de l'épisode";
-		$this->page->stylesheets[] = '/assets/css/episode.css';
+		$page = new Page;
+		$page->title = "Nom de l'épisode";
+		$page->addStylesheets(['/assets/css/episode.css']);
+		$page->addScripts([
+			'src' => 'https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=kivzmmaltnur462zqk88udo27pcq653plylb48017r3cq75i',
+			'execute' => '',
+		]);
+		$page->addCustomBtmScripts(["tinymce.init({ selector: '#episode-content' });"]);
 
 		$db = new \PDO('mysql:host=localhost;dbname=project3', 'root', 'root');
 
 		$episodes_mapper = new Mapper\Episodes($db);
 
-		$episodeTemplate = new Template('episode');
+		$episodeTemplate = new Component('episode');
+		$episodeTemplate->user = true;
 		$episodeTemplate->episode = $episodes_mapper->get($this->HTTPRequest->GETData('number'));
 
 		$page->view = $episodeTemplate->render();
