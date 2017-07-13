ab36525c8166324aefd66e8ea4f2c35e0f7ee47f
Souhail Moussaoui <smsouhail@gmail.com>
2017-07-13 07:17:31 +0200

    Add PDOFactory to get DB Connexion

---
+++ library/QFram/helpers/PDOFactory.php
@@ -0,0 +1,13 @@
+<?php
+namespace QFram\Helper;
+
+class PDOFactory
+{
+	public static function getMysqlConnexion()
+	{
+		$db = new \PDO('mysql:host=localhost;dbname=project3', 'root', 'root');
+		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+		return $db;
+	}
+}
