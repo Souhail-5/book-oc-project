3dceb6fb461fc37581547bb02c7aac87fbfed425
Souhail Moussaoui <smsouhail@gmail.com>
2017-07-18 15:32:46 +0200

    Add Comment Model

---
+++ app/models/domain-objects/Comment.php
@@ -0,0 +1,66 @@
+<?php
+namespace Model\Object;
+
+/**
+* Comment Model
+*/
+class Comment
+{
+	protected $id;
+	protected $name;
+	protected $email;
+	protected $text;
+	protected $publishDatetime;
+	protected $signal;
+
+	public function __construct(array $data = [])
+	{
+		$this->hydrate($data);
+	}
+
+	// To-do: transform it to a Trait
+	public function hydrate(array $data)
+	{
+		foreach ($data as $property => $value) {
+			$method = 'set'.ucfirst($property);
+			if (method_exists($this, $method)) $this->$method($value);
+		}
+	}
+
+	public function id() { return $this->id; };
+	public function name() { return $this->name; };
+	public function email() { return $this->email; };
+	public function text() { return $this->text; };
+	public function publishDatetime() { return $this->publishDatetime; };
+	public function signal() { return $this->signal; };
+
+	protected function setId($value)
+	{
+		$this->id = $value;
+	}
+
+	protected function setName($value)
+	{
+		$this->name = $value;
+	}
+
+	protected function setEmail($value)
+	{
+		$this->email = $value;
+	}
+
+	protected function setText($value)
+	{
+		$this->text = $value;
+	}
+
+	protected function setPublishDatetime($value)
+	{
+		$this->publishDatetime = $value;
+	}
+
+	protected function setSignal($value)
+	{
+		$this->signal = $value;
+	}
+}
