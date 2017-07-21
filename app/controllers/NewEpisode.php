1603470a9eaa37e7f1be4a500b3a9c4b7a553865
Souhail Moussaoui <smsouhail@gmail.com>
2017-07-21 16:22:24 +0200

    Add New Episode Controller and View

---
+++ app/controllers/NewEpisode.php
@@ -0,0 +1,74 @@
+<?php
+namespace Controller;
+
+use \Controller\Controller;
+use \QFram\Router;
+
+/**
+* New Episode Controller
+*/
+class NewEpisode extends Controller
+{
+	protected function init()
+	{
+		// To-do: add verify authentication
+
+		$this->initServices([
+			'episodes' => 'Episodes',
+			'comments' => 'Comments',
+		]);
+		$this->initPage();
+		$this->initComponents([
+			'new-episode' => 'new-episode',
+		]);
+	}
+
+	public function render()
+	{
+		$this->page->title = "Nouvel épisode";
+		$this->page->bodyId = "episode";
+		$this->page->addScripts([
+			'<script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=kivzmmaltnur462zqk88udo27pcq653plylb48017r3cq75i"></script>',
+		]);
+		$this->page->addCustomBtmScripts(["tinymce.init({
+			selector: '.editable-content',
+			inline: true,
+			theme: 'inlite',
+			selection_toolbar: 'bold italic | blockquote h2 h3',
+			insert_toolbar: '',
+			branding: false
+		});"]);
+
+		$this->page->view = $this->getComponent('new-episode')->render();
+
+		echo $this->page->render();
+	}
+
+	public function show()
+	{
+		$this->getComponent('new-episode')->episode = $this->getService('episodes')->setNewEpisode();
+
+		$this->render();
+	}
+
+	public function newEpisode()
+	{
+		$this->getComponent('new-episode')->episode = $this->getService('episodes')->setNewEpisode([
+			'number' => $this->HttpRequest->POSTData('mce_0'),
+			'part' => $this->HttpRequest->POSTData('mce_1'),
+			'title' => $this->HttpRequest->POSTData('mce_2'),
+			'text' => $this->HttpRequest->POSTData('mce_3'),
+		]);
+
+		try {
+			$this->getService('episodes')->add($this->getComponent('new-episode')->episode);
+
+			$this->getComponent('new-episode')->episode = $this->getService('episodes')->getEpisode($this->getComponent('new-episode')->episode);
+
+			$this->HttpResponse->redirect(Router::getPath('episode', [$this->getComponent('new-episode')->episode->number(), $this->getComponent('new-episode')->episode->slug()]));
+		} catch (\Exception $e) {
+			echo $e->getMessage();
+			$this->render();
+		}
+	}
+}
--- app/views/new-episode.php
+++ app/views/new-episode.php
@@ -12,37 +12,23 @@
 				</ol>
 				<form action="" method="POST">
 					<header class="text-center">
-						<span class="badge badge-default"># <span class="editable-content"><?= $episode->number() ?></span></span>
-						<span class="badge badge-pill badge-default editable-content"><?= $episode->part() ?></span>
-						<h1 class="editable-content"><?= $episode->title() ?></h1>
+						<span class="badge badge-default"># <span class="editable-content">X<?= $episode->number() ?></span></span>
+						<span class="badge badge-pill badge-default editable-content">0<?= $episode->part() ?></span>
+						<h1 class="editable-content">Titre de l'épisode <?= $episode->title() ?></h1>
 					</header>
-					<div class="text-center">
-						<time datetime="YYYY-MM-DD">
-							<svg xmlns="http://www.w3.org/2000/svg" class="si-glyph-calendar-1">
-								<use xlink:href="sprite.svg#si-glyph-calendar-1">
-							</svg>
-							<?= $episode->modificationDatetime() ?>
-						</time>
-						<a href="#"><?= $episode->nbrComments() ?> commentaires</a>
-					</div>
 					<div class="text-center">
 						<button class="act-delete" type="submit" name="action" value="delete-episode">
 							Supprimer
 						</button>
 						<button type="submit" name="action" value="new-episode">
-							Enregistrer
+							Publier
 						</button>
 					</div>
 					<div class="editable-content episode-text" placeholder="test" style="min-height: 150px;">
-						<?= $episode->text() ?>
+						Votre texte <?= $episode->text() ?>
 					</div>
 				</form>
 			</article>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-10 offset-md-1 bg-white px-5">
-			<?= $new_comment_form ?>
 		</div>
 	</div>
 </div>
