97498f534f5d2269932ff1e706b015f790bc1380
Souhail Moussaoui <smsouhail@gmail.com>
2017-08-06 03:29:36 +0200

    Clean up

--- app/views/comment.php
+++ app/views/comment.php
@@ -4,7 +4,7 @@
 		<div class="d-flex justify-content-between align-items-center">
 			<h6 class="my-0 fz-1-05"><?= $comment->name() ?></h6>
 			<form class="d-flex align-items-center" action="" method="POST">
-				<?php if ($comment->approved() == 0 && ($current_route->name() == 'episode' || $current_route->name() == 'latest-episode')): ?>
+				<?php if ($comment->approved() == 0 && $current_route->name() == 'episode'): ?>
 					<button class="btn btn-link p-0 ml-4 meta-danger fz-0-95" type="submit" name="action" value="signal-comment">
 						<svg xmlns="http://www.w3.org/2000/svg" class="si-glyph-circle-remove wh-0-95">
 							<use xlink:href="/sprite.svg#si-glyph-circle-remove">
