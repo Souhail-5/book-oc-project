86e1af692b9107832c821d42d503fcc6cf29133b
Souhail Moussaoui <smsouhail@gmail.com>
2017-08-06 06:16:37 +0200

    Add hydrate() as Trait

--- library/QFram/helpers/Utility.php
+++ library/QFram/helpers/Utility.php
@@ -3,6 +3,14 @@ namespace QFram\Helper;
 
 trait Utility
 {
+	public function hydrate(array $data)
+	{
+		foreach ($data as $property => $value) {
+			$method = 'set'.ucfirst($property);
+			if (method_exists($this, $method)) $this->$method($value);
+		}
+	}
+
 	public function _ifNotEmpty($test, $notEmpty, $empty='')
 	{
 		if (!empty($test)) {
