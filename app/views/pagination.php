fe18f7be72d1923e5db75d249d55f9490f2ad707
Souhail Moussaoui <smsouhail@gmail.com>
2017-08-08 02:59:50 +0200

    Fix pagination view

--- app/views/pagination.php
+++ app/views/pagination.php
@@ -2,10 +2,10 @@
 	<nav>
 		<ul class="pagination justify-content-center">
 			<?php for ($i = 1; $i <= ceil($nbr_elements / $elements_limit_by_page); $i++): ?>
-				<?php if ((($current_route->name() == 'episodes' || $current_route->name() == 'root') && $i == 1) || (isset($current_route->vars()['page']) && $i == $current_route->vars()['page'])): ?>
+				<?php if ((!isset($current_route->vars()['page']) && $i == 1) || (isset($current_route->vars()['page']) && $i == $current_route->vars()['page'])): ?>
 					<li class="page-item active"><span class="page-link"><?= $i ?></span><span class="sr-only">(active)</span></li>
 				<?php else: ?>
-					<li class="page-item"><a class="page-link" href="<?= $path('episodes-page', [$i]) ?>"><?= $i ?></a></li>
+					<li class="page-item"><a class="page-link" href="<?= $current_route->name() == 'root' ? $path('episodes', [$i]) : $path($current_route->name(), [$i]) ?>"><?= $i ?></a></li>
 				<?php endif; ?>
 			<?php endfor; ?>
 		</ul>
