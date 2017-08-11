72a228cf5a2fdfd81567fe1eddfd0df26ba035ac
Souhail Moussaoui <smsouhail@gmail.com>
2017-08-11 14:15:06 +0200

    FIX log-out icons

--- app/views/page.php
+++ app/views/page.php
@@ -13,9 +13,9 @@
 <body id="<?= $bodyId ?>">
 	<?php if ($user->isAuthenticated()): ?>
 		<nav class="navbar fixed-top navbar-inverse bg-inverse zi-50 flex-row justify-content-between align-items-center py-1 fz-0-95">
-			<span class="text-white">Mode administration</span>
-			<button class="btn btn-link meta-danger p-0 m-0" form="form-log-out" type="submit" name="action" value="sign-out">
-				<svg-icon class="si-glyph-turn-off meta">
+			<span class="text-white fz-0-80">Mode administration</span>
+			<button class="btn btn-link meta-danger p-0 m-0 fz-0-80" form="form-log-out" type="submit" name="action" value="sign-out">
+				<svg-icon class="si-glyph-turn-off meta wh-0-80">
 					<src href="/sprite.svg#si-glyph-turn-off" />
 				</svg-icon>
 				Se déconnecter
